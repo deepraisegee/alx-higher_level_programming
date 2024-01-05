db86e2a0e4281d70680e1ddb593fa47ad7fb5e16
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2024-01-05 15:48:58 +0000

    Third task added

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ argv_len = len(argv) - 1
 
 if __name__ == "__main__":
     if argv_len == 0:
-        print(".")
+        print("0 arguments.")
     elif argv_len == 1:
         print("{} argument:".format(argv_len))
     elif argv_len > 1:
