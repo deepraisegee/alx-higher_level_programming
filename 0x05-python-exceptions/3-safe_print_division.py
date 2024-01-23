e989844f601b6184503207c61bc60e8f47decb53
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2024-01-23 00:49:17 +0000

    0x05-python-exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,9 @@
 def safe_print_division(a, b):
     try:
         result = a / b
+        return result
     except ZeroDivisionError:
         result = None
+        return result
     finally:
         print("Inside result: {}".format(result))
