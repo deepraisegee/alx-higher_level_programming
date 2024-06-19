0859f92be55f0ef4585858a0dc48fab9ddca4611
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2024-06-19 16:37:37 +0000

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,4 +2,4 @@
 
 const argv = process.argv.map(x => Number(x)).slice(2);
 
-if (argv.length <= 1) { console.log(1); } else { console.log(argv.sort((a, b) => b - a)[1]); }
+if (argv.length <= 1) { console.log(0); } else { console.log(argv.sort((a, b) => b - a)[1]); }
