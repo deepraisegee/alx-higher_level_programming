bf45ade8356cec4adb2a50bc75d11d8798d352cf
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2024-06-19 14:25:07 +0000

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 const argv = process.argv;
-if (Number(argv[2])) { console.log(Number(argv[2]).toFixed()); } else { console.log('Not a number'); }
+if (Number(argv[2])) { console.log(`My number: ${Number(argv[2]).toFixed()}`); } else { console.log('Not a number'); }
