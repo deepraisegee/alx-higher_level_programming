13302774c5e2043d0da33afe02690b3bc0509475
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2024-06-21 14:53:26 +0000

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,19 @@
 #!/usr/bin/node
 
 const { dict } = require('./101-data.js');
-const newObj = {};
-Object.values(dict).forEach(x => Object.keys(dict));
+
+const userIdsByOccurrence = {};
+
+for (const [userId, occur] of Object.entries(dict)) {
+  if (!userIdsByOccurrence[occur]) {
+    userIdsByOccurrence[occur] = [];
+  }
+  userIdsByOccurrence[occur].push(userId);
+}
+
+const finalResult = {};
+for (const [occur, userIds] of Object.entries(userIdsByOccurrence)) {
+  finalResult[occur.toString()] = userIds;
+}
+
+console.log(finalResult);
