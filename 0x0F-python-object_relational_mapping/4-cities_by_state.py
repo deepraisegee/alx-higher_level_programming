6254b949ffaf0da857ed354b5775f00b43287ce6
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2024-06-23 23:36:19 +0000

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+An introduction to MySQLdb Python lybrary
+with MySQL database.
+"""
+import sys
+
+import MySQLdb
+
+
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    query = sys.argv[4]
+
+    # connect to MySQL db
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name,
+            charset="utf8"
+        )
+    cur = db.cursor()
+
+    # execute sql query
+    cur.execute(
+          "SELECT * FROM cities"
+        )
+    states = cur.fetchall()
+
+    # close the cursor
+    cur.close()
+
+    for state in states:
+        print(state)
+
+
+if __name__ == "__main__":
+    main()
