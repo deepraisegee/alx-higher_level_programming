415ddc2d73a35feef2c815334a945c824c4b5c3e
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2024-06-23 17:58:40 +0000

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+An introduction to MySQLdb Python lybrary
+with MySQL database.
+"""
+import sys
+
+import MySQLdb
+
+
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # connect to MySQL db
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name,
+            charset="utf8"
+        )
+    cur = db.cursor()
+
+    # execute sql query
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%'"
+    )
+    states = cur.fetchall()
+
+    # close the cursor
+    cur.close()
+
+    for state in states:
+        print(state)
+
+
+if __name__ == "__main__":
+    main()
